aa5df2cb02edd8535653b4cf242f35fa4e122478
Matthew Cavallo <macavall@microsoft.com>
2024-04-12 14:18:17 -0500

    first

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using Raylib_cs;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Init Raylib game window
+            Raylib.InitWindow(800, 600, "Raylib C# Example");
+
+            // Set Window Background Color
+
+            // Game Loop
+            while (!Raylib.WindowShouldClose())
+            {
+                // Update
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.White);
+                Raylib.DrawText("Hello, World!", 10, 10, 20, Color.Black);
+                Raylib.EndDrawing();
+            }
+
+            // Close Window
+            Raylib.CloseWindow();
+        }
+    }
+}
